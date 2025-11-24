227cdb346fe60e4df360af3654fef724269cfe5f
Julen Mendieta <jmendieta@milaboratories.com>
2025-11-24 10:32:22 +0100

    Pairing wont fail if there are no DA clonotypes in one chain

--- software/src/tcr-disco/find-pairs.R
+++ software/src/tcr-disco/find-pairs.R
@@ -155,13 +155,13 @@ fdr_cut <- opt$p_threshold
 output_folder <- opt$output
 
 # test
-# metadata <- "/Users/julen/Downloads/miltenyi_test/oncolumn_TCR_discovery/platforma/0x5B60C6/forPairing/metadata.tsv"
-# main_alpha <- "/Users/julen/Downloads/miltenyi_test/oncolumn_TCR_discovery/platforma/0x5B60C6/forPairing/mainAlpha.tsv"
-# main_beta <- "/Users/julen/Downloads/miltenyi_test/oncolumn_TCR_discovery/platforma/0x5B60C6/forPairing/mainBeta.tsv"
-# da_alpha <- "/Users/julen/Downloads/miltenyi_test/oncolumn_TCR_discovery/platforma/0x5B60C6/forPairing/daAlpha.csv"
-# da_beta <- "/Users/julen/Downloads/miltenyi_test/oncolumn_TCR_discovery/platforma/0x5B60C6/forPairing/daBeta.csv"
+# metadata <- "/Users/julen/Downloads/m_test/oncolumn_TCR_discovery/platforma/0x5B60C6/forPairing/metadata.tsv"
+# main_alpha <- "/Users/julen/Downloads/m_test/oncolumn_TCR_discovery/platforma/0x5B60C6/forPairing/mainAlpha.tsv"
+# main_beta <- "/Users/julen/Downloads/m_test/oncolumn_TCR_discovery/platforma/0x5B60C6/forPairing/mainBeta.tsv"
+# da_alpha <- "/Users/julen/Downloads/m_test/oncolumn_TCR_discovery/platforma/0x5B60C6/forPairing/daAlpha.csv"
+# da_beta <- "/Users/julen/Downloads/m_test/oncolumn_TCR_discovery/platforma/0x5B60C6/forPairing/daBeta.csv"
 # contrast_col <- "ag"
-# output_folder <- "/Users/julen/Downloads/miltenyi_test/oncolumn_TCR_discovery/platforma/0x5B60C6/resultsPairing"
+# output_folder <- "/Users/julen/Downloads/m_test/oncolumn_TCR_discovery/platforma/0x5B60C6/resultsPairing"
 # sample_id_col <- "Barcode ID"
 
 # Get from platforma
@@ -169,25 +169,7 @@ output_folder <- opt$output
 # fdr_cut <- 0.05
 estimate_cut <- 0.95
 
-## 1.1. TCR Discovery
-# Load metadata
-metadata_table <- read.table(metadata, header = TRUE, sep = "\t", stringsAsFactors = FALSE, check.names = FALSE)
-main_alpha_table <- read.table(main_alpha, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-main_beta_table <- read.table(main_beta, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-
-# check if sample_id_col is in metadata_table and if it has less values than internalSampleId
-main_alpha_table$useSampleId <- main_alpha_table$internalSampleId
-main_beta_table$useSampleId <- main_beta_table$internalSampleId
-if (sample_id_col %in% colnames(metadata_table)) {
-  if (length(unique(metadata_table[, sample_id_col])) < length(unique(metadata_table$internalSampleId))) {
-    # Add original sample ID to the alpha and beta tables
-    main_alpha_table$useSampleId <- metadata_table[match(main_alpha_table$internalSampleId, metadata_table$internalSampleId), sample_id_col]
-    main_beta_table$useSampleId <- metadata_table[match(main_beta_table$internalSampleId, metadata_table$internalSampleId), sample_id_col]
-
-    metadata_table$useSampleId <- metadata_table[, sample_id_col]
-  } 
-}
-
+## Control prints
 print(paste("metadata file: ", metadata))
 print(paste("main alpha file: ", main_alpha))
 print(paste("main beta file: ", main_beta))
@@ -198,41 +180,74 @@ print(paste("sample id column: ", sample_id_col))
 print(paste("fc threshold: ", fc_cut))
 print(paste("fdr threshold: ", fdr_cut))
 print(paste("output folder: ", output_folder))
-# Make sure we have the same set of samples to compare
-if (!identical(sort(unique(main_alpha_table$useSampleId)), sort(unique(main_beta_table$useSampleId)))) {
-  stop("Error: The sets of samples in the alpha and beta tables are not the same")
-}
-# Get alpha/beta fraction matrices
-alpha_matrix <- get_fraction_matrix(main_alpha_table)
-beta_matrix <- get_fraction_matrix(main_beta_table)
 
+## 1.1. TCR Discovery
 # Get alpha/beta DEG tables
 deg_alpha_table <- read.csv(da_alpha, header = TRUE, sep = ",", stringsAsFactors = FALSE)
 deg_beta_table <- read.csv(da_beta, header = TRUE, sep = ",", stringsAsFactors = FALSE)
-# Get all possible values from Numerator column in both tables
-numerators <- unique(c(deg_alpha_table$Numerator, deg_beta_table$Numerator))
 
-# Find pairs per numerator and comine results into one table
-predicted_pairs_all <- data.frame()
-for (num in numerators) {
-  predicted_pairs <- find_pairs(deg_alpha_table, deg_beta_table, metadata_table, 
-      contrast_col, alpha_matrix, beta_matrix, num)
-  predicted_pairs_all <- rbind(predicted_pairs_all, predicted_pairs)
+if (nrow(deg_alpha_table) == 0 || nrow(deg_beta_table) == 0) {
+  print("Warning: The DA alpha or beta tables are empty. No pairs will be found.")
+
+  # Create an emtpy output table with sall the required columns
+  required_cols <- c("Contrast", "tra", "trb", "estimate", "p.value", "p.adj", "tra_CDR3aa", "tra_VGene", "trb_CDR3aa", "trb_VGene")
+  empty_table <- data.frame(matrix(ncol = length(required_cols), nrow = 0))
+  colnames(empty_table) <- required_cols
+  predicted_pairs_all <- empty_table
+
+} else {
+  # Load metadata and main alpha/beta tables
+  metadata_table <- read.table(metadata, header = TRUE, sep = "\t", stringsAsFactors = FALSE, check.names = FALSE)
+  main_alpha_table <- read.table(main_alpha, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+  main_beta_table <- read.table(main_beta, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+
+  # check if sample_id_col is in metadata_table and if it has less values than internalSampleId
+  main_alpha_table$useSampleId <- main_alpha_table$internalSampleId
+  main_beta_table$useSampleId <- main_beta_table$internalSampleId
+  if (sample_id_col %in% colnames(metadata_table)) {
+    if (length(unique(metadata_table[, sample_id_col])) < length(unique(metadata_table$internalSampleId))) {
+      # Add original sample ID to the alpha and beta tables
+      main_alpha_table$useSampleId <- metadata_table[match(main_alpha_table$internalSampleId, metadata_table$internalSampleId), sample_id_col]
+      main_beta_table$useSampleId <- metadata_table[match(main_beta_table$internalSampleId, metadata_table$internalSampleId), sample_id_col]
+
+      metadata_table$useSampleId <- metadata_table[, sample_id_col]
+    } 
+  }
+
+  # Make sure we have the same set of samples to compare
+  if (!identical(sort(unique(main_alpha_table$useSampleId)), sort(unique(main_beta_table$useSampleId)))) {
+    stop("Error: The sets of samples in the alpha and beta tables are not the same")
+  }
+  # Get alpha/beta fraction matrices
+  alpha_matrix <- get_fraction_matrix(main_alpha_table)
+  beta_matrix <- get_fraction_matrix(main_beta_table)
+
+
+  # Get all possible values from Numerator column in both tables
+  numerators <- unique(c(deg_alpha_table$Numerator, deg_beta_table$Numerator))
+
+  # Find pairs per numerator and comine results into one table
+  predicted_pairs_all <- data.frame()
+  for (num in numerators) {
+    predicted_pairs <- find_pairs(deg_alpha_table, deg_beta_table, metadata_table, 
+        contrast_col, alpha_matrix, beta_matrix, num)
+    predicted_pairs_all <- rbind(predicted_pairs_all, predicted_pairs)
+  }
+
+  # Filter out negative correlations
+  predicted_pairs_all <- predicted_pairs_all[predicted_pairs_all$estimate >= 0, ]
+
+  # Add TRA and TRB CDR3 aa and VGene data
+  # Match tra column with alpha table (match returns first occurrence, which is fine since CDR3aa/VGene are always the same for repeated clonotypeKeys)
+  alpha_match_idx <- match(predicted_pairs_all$tra, main_alpha_table$clonotypeKey)
+  predicted_pairs_all$tra_CDR3aa <- main_alpha_table$CDR3aa[alpha_match_idx]
+  predicted_pairs_all$tra_VGene <- main_alpha_table$VGene[alpha_match_idx]
+
+  # Match trb column with beta table
+  beta_match_idx <- match(predicted_pairs_all$trb, main_beta_table$clonotypeKey)
+  predicted_pairs_all$trb_CDR3aa <- main_beta_table$CDR3aa[beta_match_idx]
+  predicted_pairs_all$trb_VGene <- main_beta_table$VGene[beta_match_idx]
 }
-
-# Filter out negative correlations
-predicted_pairs_all <- predicted_pairs_all[predicted_pairs_all$estimate >= 0, ]
-
-# Add TRA and TRB CDR3 aa and VGene data
-# Match tra column with alpha table (match returns first occurrence, which is fine since CDR3aa/VGene are always the same for repeated clonotypeKeys)
-alpha_match_idx <- match(predicted_pairs_all$tra, main_alpha_table$clonotypeKey)
-predicted_pairs_all$tra_CDR3aa <- main_alpha_table$CDR3aa[alpha_match_idx]
-predicted_pairs_all$tra_VGene <- main_alpha_table$VGene[alpha_match_idx]
-
-# Match trb column with beta table
-beta_match_idx <- match(predicted_pairs_all$trb, main_beta_table$clonotypeKey)
-predicted_pairs_all$trb_CDR3aa <- main_beta_table$CDR3aa[beta_match_idx]
-predicted_pairs_all$trb_VGene <- main_beta_table$VGene[beta_match_idx]
 
 #save ft_ and ct_filtered in the output_folder
 write.table(predicted_pairs_all, paste0(output_folder, "/ab_pairs.tsv"), 
