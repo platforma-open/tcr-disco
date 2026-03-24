476a7a90b39d8330077a8e017912c5f3534bc344
Julen Mendieta <jmendieta@milaboratories.com>
2026-03-24 16:38:29 +0100

    Update SDK

--- ui/src/pages/FrequenciesHeatmapPage.vue
+++ ui/src/pages/FrequenciesHeatmapPage.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import type { PredefinedGraphOption } from '@milaboratories/graph-maker';
 import { GraphMaker } from '@milaboratories/graph-maker';
-import '@milaboratories/graph-maker/styles';
 import type { PColumnIdAndSpec } from '@platforma-sdk/model';
 import { plRefsEqual } from '@platforma-sdk/model';
 import { PlTabs } from '@platforma-sdk/ui-vue';
